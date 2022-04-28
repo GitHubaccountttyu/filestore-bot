3fdc84b2bc362c8dab4e095e3db3689cd99f7831
GitHubaccountttyu <101639141+GitHubaccountttyu@users.noreply.github.com>
2022-04-28 22:29:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-# (c) @AbirHasan2005
+# (c) @JamesbonddOO7
 
 import os
 
@@ -30,20 +30,20 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @JamesbonddOO7
 
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
+👥 **Support Group:** [Linux Repositories](https://t.me/JamesbonddOO7)
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+📢 **Updates Channel:** [https://t.me/JamesbonddOO7](https://t.me/JamesbonddOO7)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @JamesbonddOO7
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
